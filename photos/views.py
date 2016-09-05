c8a8548be9d8b3eeeed2dea19084ae24413a9c85
Daniel Martínez Piñero <dmpinero@gmail.com>
2016-09-05 13:57:20 +0200

    Filtrado de fotos según visibilidad

--- photos/views.py
+++ photos/views.py
@@ -3,10 +3,10 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render
-from photos.models import Photo
+from photos.models import Photo, PUBLIC
 
 def home(request):
-    photos = Photo.objects.all().order_by('-created_at') # Devuelve todas las fotos a través del ModelManager. # Se configura la query, ordenadación descentente por fecha de creación
+    photos = Photo.objects.filter(visibility=PUBLIC).order_by('-created_at') # Devuelve todas las fotos a través del ModelManager. # Se configura la query, ordenadación descentente por fecha de creación
     context = {
         'photos_list': photos[:5] # Limita la query. Sólo se ejecuta cuando la variable va a ser utilizada
     }
