82ccd21d37c4ae26a80e16683b24df318211acb1
Daniel Martínez Piñero <dmpinero@gmail.com>
2016-09-22 17:47:58 +0200

    REST con vistas de Django (APIView)

--- users/api.py
+++ users/api.py
@@ -1,11 +1,9 @@
 from django.contrib.auth.models import User
-from django.http import HttpResponse
-from django.views.generic import View
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from users.serializers import UserSerializer
-from rest_framework.renderers import JSONRenderer
 
-
-class UserListAPI(View):
+class UserListAPI(APIView):
     """
     Obtiene los usuarios del sistemas
     El serializador transforma la lista de objetos User a un diccionario de datos en formato JSON
@@ -14,7 +12,5 @@ class UserListAPI(View):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True) # Serializa todos los objetos que se le pasan (al ser más de
                                                       # uno es necesario poner many=True
-        serialized_users = serializer.data  # Lista de diccionarios
-        renderer = JSONRenderer()
-        json_users = renderer.render(serialized_users) # Transforma la lista de diccionarios a formato JSON
-        return HttpResponse(json_users)
+        serialized_users = serializer.data            # Lista de diccionarios
+        return Response(serialized_users)
