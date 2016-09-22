4ad4ec019590148ee19bd4687e29b237582fcb70
Daniel Martínez Piñero <dmpinero@gmail.com>
2016-09-22 18:03:34 +0200

    REST con vistas de Django (APIView) y metodo get_object_or_404

--- frikr/urls.py
+++ frikr/urls.py
@@ -4,7 +4,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from photos.views import HomeView, DetailView, CreateView
-from users.api import UserListAPI
+from users.api import UserListAPI, UserDetailAPI
 from users.views import LoginView, LogoutView
 
 urlpatterns = [
@@ -23,5 +23,6 @@ urlpatterns = [
     url(r'^logout$', LogoutView.as_view(), name="users_logout"),
 
     # Users API URLs
-    url(r'^api/1.0/users/$', UserListAPI.as_view(), name='user_list_api')
+    url(r'^api/1.0/users/$', UserListAPI.as_view(), name='user_list_api'),
+    url(r'^api/1.0/users/(?P<pk>[0-9]+)$', UserDetailAPI.as_view(), name='user_detail_api')
 ]
--- users/api.py
+++ users/api.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from users.serializers import UserSerializer
+from django.shortcuts import get_object_or_404
 
 class UserListAPI(APIView):
     """
@@ -14,3 +15,14 @@ class UserListAPI(APIView):
                                                       # uno es necesario poner many=True
         serialized_users = serializer.data            # Lista de diccionarios
         return Response(serialized_users)
+
+class UserDetailAPI(APIView):
+    """
+    Obtiene los usuarios del sistemas
+    El serializador transforma la lista de objetos User a un diccionario de datos en formato JSON
+    """
+    def get(self, request, pk):
+        user = get_object_or_404(User, pk=pk)  # Busca el usuario por clave primaria
+        serializer = UserSerializer(user)      # Convierte el objeto en un diccionario. Lo almacena en un atributo data
+
+        return Response(serializer.data)
