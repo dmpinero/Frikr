6760a2e9036945349e4fc043c0e0977ab6997503
Daniel Martínez Piñero <dmpinero@gmail.com>
2016-09-23 08:47:59 +0200

    REST. Endpoint de detalle, actualizacion y borrado  de fotos con Vistas Genericas (RetrieveUpdateDestroyAPIView)

--- photos/api.py
+++ photos/api.py
@@ -2,8 +2,13 @@ from rest_framework.views import APIView
 
 from photos.models import Photo
 from photos.serializers import PhotoSerializer
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
 
 class PhotoListAPI(ListCreateAPIView):
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer  # Hay que indicarle la clase. No hay que instanciarla
+
+class PhotoDetailAPI(RetrieveUpdateDestroyAPIView):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer  # Hay que indicarle la clase. No hay que instanciarla
